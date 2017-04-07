42961a9d96b89381fc41d653fe2449f46bbec625
AlucardFAS <FernandoImp@outlook.com.br>
2017-04-07 17:23:42 -0300

    Add files via upload

---
+++ URI 1173 resolvido c/uri.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int n[10],i;
+	scanf("%d", &n[0]);
+	printf("N[0] = %d\n",n[0]);
+	
+	for(i=1;i<10;i++)
+	{
+		n[i]=2*n[i-1];
+		printf("N[%d] = %d\n",i,n[i]);
+	}
+	
+	
+	return 0;
+}
+
