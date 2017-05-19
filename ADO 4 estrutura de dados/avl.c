a846b7159fbaefd4f0ce5ef0dbe0d38e7867cd9a
AlucardFAS <FernandoImp@outlook.com.br>
2017-05-19 17:18:21 -0300

    inserção, busca e checagem

--- ADO 4 estrutura de dados/avl.c
+++ ADO 4 estrutura de dados/avl.c
@@ -14,7 +14,15 @@ typedef struct noh
 
 noh_t *encontre(noh_t *raiz, int dado)
 {
-	if (raiz == NULL) return NULL;
+	if (raiz == NULL)
+	{
+		printf("O elemento %d nao esta na arvore!\n",dado);
+		return NULL;
+	}
+	if (raiz -> dado == dado)
+	{
+		printf("O elemento %d esta na arvore!\n",dado);
+	}
 	if (dado < raiz->dado)
 		return encontre(raiz->esq, dado);
 	else if (dado > raiz->dado)
@@ -81,6 +89,8 @@ noh_t *cria_noh(int dado, noh_t *parent)
 	n->h = 1;
 	n->esq = NULL;
 	n->dir = NULL;
+	
+	printf("%d Inserido!\n",dado);
 
 	return n;
 }
@@ -109,36 +119,49 @@ noh_t *balanceia(noh_t *raiz)
         {
             girarD(raiz->dir);
             raiz = girarE(raiz);
-        }
+		}
     }
     return raiz;
 }
 
 noh_t *inserir(noh_t *raiz, int dado)
 {
+	int inserido=0;
     noh_t *atual = raiz;
     while (atual->dado != dado)
     {
         if (dado < atual->dado)
         {
-            if (atual->esq) atual = atual->esq;
-            else
+            if (atual->esq) 
+            {
+				atual = atual->esq;
+			}
+			else
             {
                 atual->esq = cria_noh(dado, atual);
                 atual = atual->esq;
+                inserido=1;
             }
         }
         else if (dado > atual->dado)
         {
-            if (atual->dir) atual = atual->dir;
+            if (atual->dir) 
+            {
+				atual = atual->dir;
+			}
             else
             {
                 atual->dir = cria_noh(dado, atual);
                 atual = atual->dir;
+                inserido = 1;
             }
         }
         else return raiz;
     }
+    if(dado == atual->dado && inserido == 0)
+    {
+    	printf("O elemento %d ja foi inserido!\n",dado);
+	}
     
     do
     {
@@ -150,6 +173,17 @@ noh_t *inserir(noh_t *raiz, int dado)
     return atual;
 }
 
+void checkavl(noh_t *raiz)
+{
+	if(altura(raiz->esq) - altura(raiz->dir) > 1 && altura(raiz->dir) - altura(raiz->esq) > 1)
+	{
+		printf("Arvore desbalanceada!\n");		
+	}
+	else
+	{
+		printf("Arvore balanceada!\n");
+	}
+}
 
 void print_avl(noh_t *noh, int elemento)
 {
@@ -173,6 +207,7 @@ int main(int argc, char *argv[])
 {
     noh_t *raiz = cria_noh(1, NULL);
     raiz = inserir(raiz, 2);
+    raiz = inserir(raiz, 2);
     raiz = inserir(raiz, 3);
     raiz = inserir(raiz, 4);
     raiz = inserir(raiz, 5);
@@ -180,11 +215,18 @@ int main(int argc, char *argv[])
     raiz = inserir(raiz, 7);
     raiz = inserir(raiz, 8);
     raiz = inserir(raiz, 9);
+    raiz = inserir(raiz, 9);
     raiz = inserir(raiz, 10);
     raiz = inserir(raiz, 11);
     raiz = inserir(raiz, 12);
     raiz = inserir(raiz, 13);
-    
+    encontre(raiz,3);
+	encontre(raiz,19);
+	checkavl(raiz);
+
+	printf("\n\n\n\n");
+
+    	
     print_arvore(raiz);
     
     return 0;
