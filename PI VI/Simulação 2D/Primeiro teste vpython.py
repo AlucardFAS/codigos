d5e772eed0ad1696c14c6b287513f30f6d0fbf13
Fernando Alves da Silva <fernando.silva@intra.labone.net>
2018-10-18 19:26:34 -0300

    Teste com VPython

---
+++ PI VI/Simulação 2D/Primeiro teste vpython.py
@@ -0,0 +1,7 @@
+from vpython import *
+
+esfera = sphere(pos = vector(-1,0,0), radius = 0.8, color = color.yellow)
+seta = arrow(pos = vector(-0.2,0,0), axis = vector(1,0,0), color = color.red)
+
+
+
